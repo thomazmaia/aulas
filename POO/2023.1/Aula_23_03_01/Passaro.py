8c64b199996b16b3136faee84359270d7bc2c2a5
Thomaz <thomaz.maia@ifce.edu.br>
2023-04-03 12:22:50 -0300

    add aula 01/03

---
+++ POO/2023.1/Aula_23_03_01/Passaro.py
@@ -0,0 +1,25 @@
+class Passaro:
+    quantidade_de_pernas = 2
+    quantidade_de_olhos = 2
+    quantidade_de_bicos = 1
+    possui_penas = True
+
+    def voar(self):
+        print("Estou voando")
+
+    def andar(self):
+        print("Estou andando")
+    
+    def comer(self):
+        print("Estou comendo")
+
+P = Passaro()
+P.voar()
+P.andar()
+P.comer()
+
+
+
+
+
+
