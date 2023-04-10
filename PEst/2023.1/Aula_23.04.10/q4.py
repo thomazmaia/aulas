6daf39655858c7ba215902d8f88ecda4c3cdba15
Thomaz <thomaz.maia@ifce.edu.br>
2023-04-10 14:17:30 -0300

    q4 de funções

---
+++ PEst/2023.1/Aula_23.04.10/q4.py
@@ -0,0 +1,7 @@
+# 4. Escreva uma função que receba o lado de um quadrado e retorne sua área (área = lado2)
+
+def calc_area_quadrado(lado):
+    return lado*lado
+
+print( calc_area_quadrado(3) )
+print( calc_area_quadrado(10) )
