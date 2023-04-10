1076855aaa90859a2374b2645a976fac6bd6fb41
Thomaz <thomaz.maia@ifce.edu.br>
2023-04-10 14:21:18 -0300

    q5 de funções

---
+++ PEst/2023.1/Aula_23.04.10/q5.py
@@ -0,0 +1,7 @@
+# 5. Escreva uma função que receba a base e a altura de um triângulo e retorne sua área (área = base x altura / 2)
+
+def calc_area_triangulo(base, altura):
+    return (base * altura)/2
+
+print( calc_area_triangulo(6, 9) )
+print( calc_area_triangulo(5, 8) )
