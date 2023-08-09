e0d95363302863a20a00d550c042022bafab5627
Thomaz <thomaz.maia@ifce.edu.br>
2023-08-09 09:02:05 -0300

    ex1

---
+++ 2023.2/PEst/aula_23_08_09/ex_format.py
@@ -0,0 +1,11 @@
+frutas = {
+    "Maçã": 1,
+    "Laranja": 2.3,
+    "Banana": 1.25,
+    "Morango": 5.0,
+    "Ameixa": 3.87,
+    "Abacaxi": 2,
+}
+
+for fruta, preco in frutas:
+    print(f"{fruta:.<20} {preco}")
