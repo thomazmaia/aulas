376f0c37e9e0de460c56b5204a52c073a4f82865
Thomaz <thomaz.maia@ifce.edu.br>
2023-08-14 10:50:21 -0300

    aula 14/08

---
+++ 2023.2/PEst/aula_23_08_14/ex_8.py
@@ -0,0 +1,10 @@
+import math
+
+x1 = float(input("X1: "))
+y1 = float(input("Y1: "))
+x2 = float(input("X2: "))
+y2 = float(input("Y2: "))
+
+d = math.sqrt((x2 - x1) ** 2 + (y1 - y2) ** 2)
+
+print(d)
