46d411354d27991b635d6b5e93467e7259e96f42
Thomaz <thomaz.maia@ifce.edu.br>
2023-08-21 10:23:12 -0300

    aula for

---
+++ 2023.2/PEst/aula_23_08_21/ex_for0.py
@@ -0,0 +1,2 @@
+for var in range(50, -1, -5):
+    print(var)
---
+++ 2023.2/PEst/aula_23_08_21/teste_while_for.py
@@ -0,0 +1,7 @@
+# Para um contador começando de 0 até 5,
+# variando de 1 em 1, faça
+
+contador = 0
+
+while contador <= 5:
+    contador += 1
