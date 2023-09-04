147817376b5fd7958e9aaf6794dab0e574598005
Thomaz <thomaz.maia@ifce.edu.br>
2023-09-04 09:26:22 -0300

    Aula 04/09

---
+++ 2023.2/IntroProg/Turma2/Aula_23_09_04/formatacao.py
@@ -0,0 +1,31 @@
+var = "Pontuação"
+print(f"{var: ^33}")
+
+nome = "João Carlos"
+pts = 90
+print(f"{nome:.<21}  {pts:>3} pontos")
+
+nome = "Maria Joaquina"
+pts = 135
+print(f"{nome:.<21}  {pts:>3} pontos")
+
+nome = "Francisco"
+pts = 80
+print(f"{nome:.<21}  {pts:>3} pontos")
+
+# nomes = ["João Carlos", "Maria Joaquia", "Francisco", "Abelardo", "Rogério", "Juliana"]
+# pontos = [90, 135, 80, 99, 85, 140, 112]
+
+# for i in range(len(nomes)):
+#     print(f"{nomes[i]:.<21}  {pontos[i]:>3} pontos")
+
+
+# print("            Pontuação:")
+
+# nome = "João Carlos"
+# pts = 90
+# print(f"{nome}.......... {pts} pontos")
+
+# nome = "Maria Joaquina"
+# pts = 135
+# print(f"{nome}.......... {pts} pontos")
