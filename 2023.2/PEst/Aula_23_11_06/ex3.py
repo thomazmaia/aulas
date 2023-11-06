ab683c739aad36e1634e60c8b876169dfc56e7de
Thomaz <thomaz.maia@ifce.edu.br>
2023-11-06 11:23:09 -0300

    Ex de CRUD diferente

---
+++ 2023.2/PEst/Aula_23_11_06/ex3.py
@@ -0,0 +1,8 @@
+# Crie um programa que simule um sistema de reservas de assentos em 
+# um cinema. Use uma matriz (lista aninhada) para representar os 
+# assentos, onde cada elemento da matriz pode representar um assento 
+# vazio ou reservado. O programa deve permitir que os clientes  
+# escolham assentos para reservar e exibir o mapa de assentos 
+# atualizado. O cliente não pode reserva um assendo já reservado.
+# Caso o cliente queira, ele deve conseguir cancelar a reserva. Faça
+# um menu adequado.
