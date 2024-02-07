fdbd5ac8bfe49fe24003b4138c50264ca1c74463
Thomaz <thomaz.maia@ifce.edu.br>
2024-02-07 16:27:10 -0300

    Questões de revisão

--- 2024.1/PEst/T1/aula_07_02_24/revisao_q2.py
+++ 2024.1/PEst/T1/aula_07_02_24/revisao_q2.py
@@ -1,7 +1,7 @@
 # Suponha que o preço de capa de um livro seja R$ 24,95, mas as livrarias recebem um desconto de 40%. O transporte custa R$ 3,00 para o primeiro exemplar e 75 centavos para cada exemplar adicional. Qual é o custo total de atacado para 60 cópias.
 
 quantidade = int(input("Quantos livros: "))
-
+    
 preco_livro = 24.95
 
 desconto = 40/100 # 0.4
