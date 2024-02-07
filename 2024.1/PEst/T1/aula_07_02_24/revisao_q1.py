24ecfa2f80bfe5b381211e2c993d8c8a76993533
Thomaz <thomaz.maia@ifce.edu.br>
2024-02-07 15:31:25 -0300

    Resolução de questões básicas

---
+++ 2024.1/PEst/T1/aula_07_02_24/revisao_q1.py
@@ -0,0 +1,8 @@
+# O volume de uma esfera com raio r é dado por 4/3pir3. Qual é o volume de uma esfera com raio 5?
+# OBS: inicie a variável r com o valor 5.
+
+r = 5
+
+volume = (4/3) * (3.14) * (r ** 3)
+
+print(volume)
---
+++ 2024.1/PEst/T1/aula_07_02_24/revisao_q2.py
@@ -0,0 +1,17 @@
+# Suponha que o preço de capa de um livro seja R$ 24,95, mas as livrarias recebem um desconto de 40%. O transporte custa R$ 3,00 para o primeiro exemplar e 75 centavos para cada exemplar adicional. Qual é o custo total de atacado para 60 cópias.
+
+quantidade = int(input("Quantos livros: "))
+
+preco_livro = 24.95
+
+desconto = 40/100 # 0.4
+
+preco_livro_com_desconto = 0.6 * preco_livro
+
+valor_frete = 3 + 0.75 * (quantidade - 1)
+
+valor_livros = quantidade * preco_livro_com_desconto
+
+valor_total = valor_frete + valor_livros
+
+print(f"O preço total é de: R$ {valor_total} sendo R$ {valor_livros} de livros e R$ {valor_frete} de tansporte.")
