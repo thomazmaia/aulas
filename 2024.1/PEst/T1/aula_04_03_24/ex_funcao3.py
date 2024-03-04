31936fbb987c699a58fb2747cf692b55e77f4705
Thomaz <thomaz.maia@ifce.edu.br>
2024-03-04 16:26:12 -0300

    Funções: param. posicionais

--- 2024.1/PEst/T1/aula_04_03_24/ex_funcao3.py
+++ 2024.1/PEst/T1/aula_04_03_24/ex_funcao3.py
@@ -2,6 +2,7 @@ def saudacao(nome : str, idade : int):
     print(f"Olá {nome}, você tem {idade} anos.")
 
 
+
 # Programa principal
 pessoa = input("Digite o nome da pessoa: ")
 idade_da_pessoa = int(input("Digite a idade da pessoa: "))
