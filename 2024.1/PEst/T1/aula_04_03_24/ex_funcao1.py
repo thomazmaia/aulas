7cb1097186c6b0605dcb7ab13e867e7b0cc4ae37
Thomaz <thomaz.maia@ifce.edu.br>
2024-03-04 16:00:10 -0300

    Funções

---
+++ 2024.1/PEst/T1/aula_04_03_24/ex_funcao1.py
@@ -0,0 +1,10 @@
+def saudacao(nome): # cabeçalho da função
+    # Corpo da função
+    print(f"Bem vindo(a) {nome}")
+
+
+# PROGRAMA PRINCIPAL:
+saudacao("XICO")
+saudacao("MARIA")
+saudacao("FRANCISCA")
+saudacao("ZÉ")
