04abcad82573807a66288163b49a6ac532fc98de
Thomaz <thomaz.maia@ifce.edu.br>
2024-03-26 14:31:10 -0300

    Operações entre strings

---
+++ 2024.1/PEst/T2/aula_26_03_24/strings3.py
@@ -0,0 +1,17 @@
+# Operações com strings
+
+# Concatenação ("soma") de strings
+str1 = "Olá"
+str2 = " "
+str3 = "amigo"
+
+res = str1 + str2 + str3
+print(res)
+
+# Repetição ("multiplicação") de strings
+str4 = "Olê "
+res = 4 * str4
+print(res)
+
+# Leia uma string do usuário e substitua o primeiro caractere da string por um asterisco "*"
+# Ex: string = "Hoje é um bom dia" vira "*oje é um bom dia"
