12a7a86aef8f1b2bb6744f664474a932eba9f111
Thomaz <thomaz.maia@ifce.edu.br>
2024-08-07 14:29:38 -0300

    Adicionar e remover elementos da lista

--- 2024.1/PEst/T2/aula_07_08_24/ex4.py
+++ 2024.1/PEst/T2/aula_07_08_24/ex4.py
@@ -1,4 +1,10 @@
 # Crie um programa que tenha a função "add_del(lista, elemento)". Essa função deve receber uma lista e um elemento. Você deve adicionar o elemento recebido caso ele não esteja na lista. Caso ele esteja você deve removê-lo.
+def add_del(lista:list, elemento):
+    if elemento in lista:
+        lista.remove(elemento)
+    else:
+        lista.append(elemento)
+    return lista
 
 L = []
 print(add_del(L, 1))
