98d8cbcbb0d1d8e4f9656c0d40afacad8dbd5e3a
Thomaz <thomaz.maia@ifce.edu.br>
2024-08-21 16:28:53 -0300

    Listas aninhadas

--- 2024.1/PEst/T1/aula_21_08_24/ex_listas_aninhadas2.py
+++ 2024.1/PEst/T1/aula_21_08_24/ex_listas_aninhadas2.py
@@ -1,1 +1,16 @@
 # Crie um programa que peça ao usuário para especificar o número de linhas e colunas de uma matriz. Em seguida, preencha a matriz com números inteiros fornecidos pelo usuário. Use listas aninhadas para representar a matriz.
+
+linhas = int(input("Linhas: "))
+colunas = int(input("Colunas: "))
+
+matriz = []
+for i in range(linhas):
+    aux = []
+    for j in range(colunas):
+        val = int(input(f"Digite o valor de  [{i}][{j}]: "))
+        aux.append(val)
+    matriz.append(aux)
+
+
+for linha in matriz:
+    print(linha)
