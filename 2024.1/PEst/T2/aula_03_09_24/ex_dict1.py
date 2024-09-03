8b1ca207c782a9d3ad3f03fdb9286138198d83f5
Thomaz <thomaz.maia@ifce.edu.br>
2024-09-03 14:16:31 -0300

    Exercício sobre dicionário

--- 2024.1/PEst/T2/aula_03_09_24/ex_dict1.py
+++ 2024.1/PEst/T2/aula_03_09_24/ex_dict1.py
@@ -1,9 +1,17 @@
 # Escreva um programa que crie um dicionário com 10 palavras e duas definições. O programa deve solicitar ao usuário cada chave e cada valor. Após adicionar as 10 palavras, o usuário poderá pesquisar e exibir sua definição correspondente.
 
-CHAVE : VALOR
-pc: personal computer
-mouse: periférico de entrada
-impressora: periférico de saída
 
+dicionario = {}
+for i in range(3):
+    chave = input(f"Digite a chave {i+1}: ")
+    valor = input(f"Digite o valor para '{chave}': ")
+    dicionario[chave] = valor
 
-dicionario[pc] -> personal computer
+print(dicionario)
+
+while True:
+    chave = input("Digite a chave: ")
+    if chave in dicionario:
+        print(dicionario[chave])
+    else:
+        print("Chave inexistente")
