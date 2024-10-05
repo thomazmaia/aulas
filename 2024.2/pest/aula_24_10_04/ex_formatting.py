c531b87af7ce81d100c970fc180758b67a89066b
Thomaz <thomaz.maia@ifce.edu.br>
2024-10-05 06:20:48 -0300

    Início do semestre 2024.2

---
+++ 2024.2/pest/aula_24_10_04/ex_formatting.py
@@ -0,0 +1,4 @@
+#print(f"{'MENU':.<20}")
+
+
+print(40 > 5 and 4 > 1)
---
+++ 2024.2/pest/aula_24_10_04/ex_print_input.py
@@ -0,0 +1,11 @@
+nome = input("Qual o seu nome: ")
+idade = int(input("Qual sua idade: "))
+
+dobro_idade = idade * 2
+
+print(f"Olá {nome}")
+
+
+
+
+print(f"Daqui a {idade} anos você terá {dobro_idade} anos")
