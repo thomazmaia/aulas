e7cd978a02e36b6df7f4ad249575aa840cd90ae7
Thomaz <thomaz.maia@ifce.edu.br>
2024-11-18 11:11:08 -0300

    exercíco sobre funções

--- 2024.2/pest/aula_24_11_18/ex1.py
+++ 2024.2/pest/aula_24_11_18/ex1.py
@@ -1,2 +1,12 @@
 # Crie uma função que receba dois parâmetros: a altura e a largura de um retângulo. Sua função deve calcular e imprimir a área desse retângulo.
 # Teste a função.
+
+
+def calc_area_retangulo(altura, largura):
+    area = altura * largura
+    print(f"{area} metros quadrados")
+
+calc_area_retangulo(2, 5)
+calc_area_retangulo(3.5, 12.54)
+calc_area_retangulo(40, 25)
+calc_area_retangulo(37.2, 98.9)
