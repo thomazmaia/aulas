459e7cf53a3daf7cebeaee9aa1f3c1c4552ee2b0
Thomaz <thomaz.maia@ifce.edu.br>
2024-12-09 11:14:33 -0300

    Aula 1 de strings

---
+++ 2024.2/pest/aula_24_12_09/exercicios.py
@@ -0,0 +1,37 @@
+# 1 - Leia uma string do usuário e escreva na tela o tamanho dessa string.
+# str = input("Digite alguma coisa: ")
+# tamanho = len(str)
+# print(f"{tamanho} caracteres")
+
+# 2 - Leia duas strings do usuário e escreva na tela qual das duas é a maior string.
+# str1 = input("Digite algo: ")
+# str2 = input("Digite algo: ")
+# tamanho1 = len(str1)
+# tamanho2 = len(str2)
+# if tamanho1 > tamanho2:
+#     print(f"{str1} é maior")
+# elif tamanho1 < tamanho2:
+#     print(f"{str2} é maior")
+
+# 3 - Leia uma string do usuário e mostre o primeiro caractere dessa string.
+# str = input("Digite uma string: ")
+# print(str[0])
+
+# 4 - Leia uma string do usuário e mostre essa string caractere-a-caractere (um caractere por linha) usando FOR.
+# # 1a maneira:
+# str = input("Digite algo: ")
+# for indice in range(0, len(str), 1):
+#     print(str[indice])
+# # 2a maneira:
+# str = input("Digite algo: ")
+# for item in str:
+#     print(item)
+
+# 5 - Leia uma string do usuário e escreva na tela essa string caractere-por-caractere (um por linha) do início ao fim e, sem seguida, do fim para o início.
+# Ex: "uva"
+# u
+# v
+# a
+# a
+# v
+# u
