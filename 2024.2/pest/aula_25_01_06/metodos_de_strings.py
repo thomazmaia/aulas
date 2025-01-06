0381825d4deb093dedea2f6aa6d1501f8e5b8387
Thomaz <thomaz.maia@ifce.edu.br>
2025-01-06 11:21:57 -0300

    Métodos de strings

---
+++ 2024.2/pest/aula_25_01_06/metodos_de_strings.py
@@ -0,0 +1,10 @@
+# Métodos de strings
+# Métodos -> São como funções associadas a uma biblioteca criada por "alguém" para fins específicos associadas a um tipo de dado específico (como strings, por exemplo)
+
+aux = "Abacaxi"
+print(aux.capitalize())
+print(aux.title())
+print(aux.upper())
+print(aux.lower())
+print(aux.isupper())
+print(aux.islower())
