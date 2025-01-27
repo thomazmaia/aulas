8283dff4f65b45aa83e162f3fb29e08ebb9dbce9
Thomaz <thomaz.maia@ifce.edu.br>
2025-01-27 14:34:46 -0300

    Eventos em JS

--- 2024.2/web1/T1/aula_25_01_27/js/script.js
+++ 2024.2/web1/T1/aula_25_01_27/js/script.js
@@ -1,13 +1,4 @@
-function soma() {
-    let valor1 = parseInt(document.querySelector("#val1").value);
-    let valor2 = parseInt(document.querySelector("#val2").value);
-    let soma = valor1 + valor2;
-    document.querySelector("#resp").textContent = "Resposta: " + soma;
-}
-
-function subtracao() {
-    let valor1 = parseInt(document.querySelector("#val1").value);
-    let valor2 = parseInt(document.querySelector("#val2").value);
-    let soma = valor1 - valor2;
-    document.querySelector("#resp").textContent = "Resposta: " + soma;
+function lightsOff(){
+    document.getElementById("body").style.backgroundColor = "yellow";
+    document.getElementById("body").style.cursor = "crosshair";
 }
