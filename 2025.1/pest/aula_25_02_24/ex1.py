57203772731768bf56da9b6b0fd4d871d03ca02f
Thomaz <thomaz.maia@ifce.edu.br>
2025-02-24 16:28:23 -0300

    Modo DEBUG

---
+++ 2025.1/pest/aula_25_02_24/ex1.py
@@ -0,0 +1,8 @@
+numero = 5
+contador = 0
+
+while contador != numero:
+    print(f"Contador: {contador}")
+    contador += 2
+
+print("Fim do código")
---
+++ 2025.1/pest/aula_25_02_24/ex2.py
@@ -0,0 +1,9 @@
+def soma(a : int, b : int):
+    res = a - b
+    return res
+
+
+x = 3
+y = 7
+resultado = soma(x, y)
+print(f"Somatório = {resultado}")
