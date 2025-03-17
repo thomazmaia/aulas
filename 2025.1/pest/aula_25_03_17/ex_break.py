5bcff2458d913cc9ab1a28c3c3fe77db02db9492
Thomaz <thomaz.maia@ifce.edu.br>
2025-03-17 16:15:59 -0300

    Revisão: while

---
+++ 2025.1/pest/aula_25_03_17/ex_break.py
@@ -0,0 +1,13 @@
+while True:
+    print(20*'-')
+    print(f"{'Calculadora':^20}") 
+    print(20*'-')
+    print("[0] Somar")
+    print("[0] Subtrair")
+    print(20*'-')
+    opcao = input("Digite uma opção ou 's' para sair: ")
+
+    if opcao == 's' or opcao == 'S':
+        break
+
+print("Até logo...")
---
+++ 2025.1/pest/aula_25_03_17/ex_continue.py
@@ -0,0 +1,9 @@
+contador = 10
+
+while contador > 0:
+    print(contador)
+    if contador == 5:
+        continue
+    contador -= 1
+
+print("Fim do laço") 
