61ada22ff6b58aa4c0bb8541868f43684c6d8a92
Thomaz Maia <thomazmaia@gmail.com>
2025-06-06 11:19:46 -0300

    Aula de strings

--- 2025.1/pest/aula_25_06_06/ex2.py
+++ 2025.1/pest/aula_25_06_06/ex2.py
@@ -3,6 +3,24 @@
 #     str2 = limão
 #     A saída deve ser "limãouva"
 
-# 2 - Crie um programa para ler uma string e modifiqueo o primeiro caractere da string para zero ("0").
+# str1 = input("Digite a primeira string: ")
+# str2 = input("Digite a segunda string: ")
+# res = str2 + str1
+# print(res)
+
+# 2 - Crie um programa para ler uma string e modifique o primeiro caractere da string para zero ("0").
+
+string = input("Digite alguma coisa: ")
+string = "0" + string[1:]
+print(string)
 
 # 3 - Leia duas strings e inverta o primeiro caractere delas. Ex: para "uva" e "limão" a saída deve ser "lva" e "uimão".
+
+str1 = input("Digite a primeira string: ")
+str2 = input("Digite a segunda string: ")
+
+aux = str1[0]
+str1 = str2[0] + str1[1:]
+str2 = aux + str2[1:]
+print(str1)
+print(str2)
