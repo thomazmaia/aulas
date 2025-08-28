01de8537f47d0c82b3d19abd454416539eec634c
Thomaz Maia <thomazmaia@gmail.com>
2025-08-28 10:30:41 -0300

    Manipulação no DOM

---
+++ 2025.2/web2/aula_25_08_28/aula.js
@@ -0,0 +1,10 @@
+// AULA DE ONTEM: SELECIONANDO ELEMENTOS
+// getElementById - Selecionar um elemento pelo ID:
+// getElementsByClassName - Seleciona elementos pela classe;
+// querySelector - Seleciona o primeiro elemento com o seletor fornecido
+// querySelectorAll - Seleciona todos os elementos com o seletor fornecido
+
+// AULA DE HOJE: MANIPULANDO CONTEÚDOS
+var obj_txt = document.querySelector(".txt");
+
+obj_txt.textContent = "OLÁ MUNDO";
