8acb6d4e97923cef9e952dad2e7ed288ceac9fce
Thomaz Maia <thomazmaia@gmail.com>
2025-09-04 11:10:27 -0300

    Manipulando CSS

---
+++ 2025.2/web2/aula_25_09_04/script.js
@@ -0,0 +1,11 @@
+function alterar(){
+    let caixa = document.getElementById("caixa"); // Tem que ser GETELEMENTBYID!
+    caixa.style.backgroundColor = "Red";
+    caixa.style.width = "10px";
+}
+
+function resetar(){
+    let caixa = document.getElementById("caixa"); // Tem que ser GETELEMENTBYID!
+    caixa.style.backgroundColor = "Yellow";
+    caixa.style.width = "200px";
+}
