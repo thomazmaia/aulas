8d54bb64cbcf13637c1188e86e1791a4849ce818
Thomaz Maia <thomazmaia@gmail.com>
2026-04-01 11:11:08 -0300

    Exemplos de funções

---
+++ 2026.1/pest2/aula_26_04_01/ex2.py
@@ -0,0 +1,12 @@
+# Crie uma função para receber a BASE e a ALTURA de um retângulo e imprima a ÁREA desse retângulo.
+# Ex: (base = 10; altura = 20)
+#.    Área = 200
+
+def calcular_area(base, altura):
+    area = base * altura
+    print(f"Área = {area}")
+
+calcular_area(10, 20)
+calcular_area(3, 45)
+calcular_area(7.35, 2)
+calcular_area(1.5, 10)
